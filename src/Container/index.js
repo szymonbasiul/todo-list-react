b96f68a74a18aaac5d9a8d9bfbcd33654d353098
Szymon Basiul <szymon.basiul@wp.pl>
2023-03-05 20:15:02 +0100

    Icons added, components formatted

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./style.css";
 
-const Container = ({children}) => (
-<div className="container">
-    {children}
-</div>
-);
+const Container = ({ children }) => <div className="container">{children}</div>;
 
 export default Container;
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./style.css";
 
-const Header = ({title}) => (
-    <h1 className="header">{title}</h1>
-);
+const Header = ({ title }) => <h1 className="header">{title}</h1>;
 
 export default Header;
