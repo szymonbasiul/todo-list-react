0481c0c929c7010a0659e5c4efd0d88b00f9d326
Szymon Basiul <szymon.basiul@wp.pl>
2023-03-23 18:42:22 +0100

    empty task validation added

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,6 +6,9 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+    if(newTaskContent === "") {
+      return;
+    }
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
   };
