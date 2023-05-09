9196eb36d4fe9720f77ba51af408c53f5aaa3335
Szymon Basiul <szymon.basiul@wp.pl>
2023-05-09 17:51:55 +0200

    GlobalStyle added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+  
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  
+  #root {
+    padding-top: 10%;
+    min-height: 100vh;
+    line-height: 1.15;
+    word-break: break-word;
+    font-family: 'Lato', sans-serif;
+    background-color: rgba(128, 128, 128, 0.168);
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
